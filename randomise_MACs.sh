36e8ad2c25a9f5ecb73e9572e2754c64cc2e6332
Shoban Chiddarth <messageshoban@gmail.com>
2025-12-22 17:45:33 +0530

    Updated network interfaces listing logic

--- randomise_MACs.sh
+++ randomise_MACs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-interfaces=$(ip -o link show | awk '$2 != "lo:" && $17 ~ /^([0-9a-f]{2}:){5}[0-9a-f]{2}$/ {sub(":", "", $2); print $2}')
+interfaces=$(ip -o link show | awk '$2 != "lo:" && $0 ~ /([0-9a-f]{2}:){5}[0-9a-f]{2}/ {sub(":", "", $2); print $2}')
 # lists all valid network interfaces that have a MAC address excluding loopback interface
 
 for interface in $interfaces; do
