3c241b8acbb490f20c547081b1c14815efcca90e
Shoban Chiddarth <messageshoban@gmail.com>
2025-12-22 17:44:54 +0530

    Added comment to describe variable

--- randomise_MACs.sh
+++ randomise_MACs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 interfaces=$(ip -o link show | awk '$2 != "lo:" && $17 ~ /^([0-9a-f]{2}:){5}[0-9a-f]{2}$/ {sub(":", "", $2); print $2}')
+# lists all valid network interfaces that have a MAC address excluding loopback interface
 
 for interface in $interfaces; do
     ip link set "$interface" down
